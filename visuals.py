20f69069c117c01454edc9b1248cc873d196e5a7
lmachara <laavanim@gmail.com>
2026-09-20 20:33:40 -0700

    Created 3D body

--- visuals.py
+++ visuals.py
@@ -2,8 +2,29 @@ from vpython import *
 # in powershell: python -m pip install -r reqs.txt
 
 scene = canvas(title="6-DOF Flight Simulator")
-plane = box( pos=vector(0, 0, 0), size=vector(3, 1, 1))
 
+plane = box(
+    pos=vector(0, 0, 0),
+    size=vector(6, 1.5, 1.5)
+)
+
+# Nose
+nose = cone(
+    pos=vector(3, 0, 0),
+    axis=vector(2, 0, 0),
+    radius=0.75
+)
+
+# Wings
+left_wing = box(
+    pos=vector(0, 0, 0),
+    size=vector(2.5, 5, 0.2)
+)
+
+# Tail
+tail = box(
+    pos=vector(-2.3, 0, 0),
+    size=vector(1, 2.5, 0.2)
+)
 while True:
     rate(60)
-    plane.pos.x += 0.1
